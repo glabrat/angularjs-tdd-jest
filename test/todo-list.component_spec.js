6a1b6185e0ce63ba46fd65e1867ca142f6b03a0c
Gonzalo Pincheira <gonzalo.pincheira@globant.com>
2017-07-11 11:20:51 -0400

    @gpincheiraa Adding the first test and babel presets configuration

--- test/todo-list.component_spec.js
+++ test/todo-list.component_spec.js
@@ -2,6 +2,20 @@ import { TodoListComponent } from 'components/TodoListComponent'
 
 describe("TodoListComponent", () => {
 
-    
+    let TodoListComponent
+    let controller
 
-});
+    before(() => {
+        angular
+            .module("Test")
+            .component(TodoListComponent);
+    })
+
+    beforeEach(inject(($componentController) => {
+        controller = $componentController('todoList')
+    }))
+
+    it("Should have a defined controller", () => {
+        expect(controller).toDefined()
+    })
+})
