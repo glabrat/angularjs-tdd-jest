92a3aee617b646accebb059731cb76dcce1ebfc3
Gonzalo Pincheira <gonzalo.pincheira@globant.com>
2017-08-20 00:26:32 -0300

    @gpincheiraa Agregando test para servicio todos.service

---
+++ test/todoService_spec.js
@@ -0,0 +1,43 @@
+import { TodoService } from "services/todo.service"
+
+describe("TodoService", () => {
+    let TodoServiceInstance
+
+    beforeAll(() => {
+        angular
+            .module("Test", [])
+            .service("TodoService", TodoService)
+    })
+
+    beforeEach(angular.mock.module("Test"))
+
+    beforeEach(inject((TodoService) => {
+        TodoServiceInstance = TodoService
+    }))
+
+    it("Should have defined", () => {
+        expect(TodoServiceInstance).toBeDefined()
+    })
+
+    it("Should be response a data object from response on getTodos method", async () => {
+        const responseMock = {
+            status: 200,
+            data: [
+                {
+                    name: "Learn Programming using component based approach",
+                    completed: true
+                },
+                {
+                    name: "Learn Machine Learning",
+                    completed: false
+                }
+            ]
+        }
+        jest.spyOn(TodoServiceInstance.http, 'get').mockReturnValueOnce(Promise.resolve(responseMock))
+
+        const serviceResponse = await TodoServiceInstance.getTodos()
+
+        expect(serviceResponse).toEqual(responseMock.data)
+    })
+
+})
