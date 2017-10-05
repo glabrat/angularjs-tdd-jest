c23c03ff8af0138a7497756b353687d50d8317c3
Gonzalo Pincheira <g.pincheira.a@gmail.com>
2017-10-05 09:50:50 -0300

    @gpincheiraa More modularity for serializer

--- test/snapshot/angularjsSnapshotFormat.js
+++ test/snapshot/angularjsSnapshotFormat.js
@@ -1,36 +1,27 @@
 import "angular";
 import "angular-mocks";
 import { domAttributesList } from "./domAttributesList"
-const HTMLNormalizer = (element, injector) => {
-    const hyphenToCamelCase = (text) => text.replace(/-(.)/g, (match, group1) => group1.toUpperCase())
-    const removeHTMLComments = (allChildrenNodes) => {
-        for(let i = 0; i < allChildrenNodes.length ; i++) {
-            if(allChildrenNodes[i].nodeType === 8) {
-                allChildrenNodes[i].parentNode.removeChild(allChildrenNodes[i]);
-            }
-        }
-        return allChildrenNodes;
-    };
+import { removeHTMLComments } from "./utils"
+
+const HTMLNormalizer = (element) => {
     const allChildrenNodes = removeHTMLComments(element[0].childNodes);
     const allChildrenElements = element[0].getElementsByTagName("*");
 
-    Array.from(allChildrenElements)
-		.forEach((child) => {
-    		Array.from(child.attributes).forEach((attr) => {
-    			if(attr.value && domAttributesList.indexOf(attr.name) === -1) {
-                    attr.value = "[ngExpression]";
-                }
-    		});
+    [...allChildrenElements].forEach((child) => {
+		[...child.attributes].forEach((attr) => {
+			if(attr.value && domAttributesList.indexOf(attr.name) === -1) {
+                attr.value = "[ngExpression]";
+            }
 		});
+	});
     return element[0];
 }
 export const angularjsSnapshotFormat = (html, scope) => {
     let el;
-    inject(($compile, $injector) => {
+    inject(($compile) => {
         el = $compile(html)(scope);
         scope.$digest();
-
-        el = HTMLNormalizer(el, $injector);
+        el = HTMLNormalizer(el);
     });
     return el.outerHTML;
 }
