34bb6fb8d5c673658f958a154891a4363d2b1fd2
Gonzalo Pincheira <g.pincheira.a@gmail.com>
2017-10-12 20:51:26 -0300

    refactor(integration): refactor integreation tests

--- __test__/integration/state_new_todo.spec.js
+++ __test__/integration/state_new_todo.spec.js
@@ -1,36 +1,31 @@
-import "index.js"
-import "angular-mocks"
+import "../../src/index";
+import "angular-mocks";
+import { jsdomUIRouterScenario } from "./jsdom-ui-router-scenario";
 
 describe("New Todo rendering and interaction on '/newtodo' path", () => {
-    const treeDOMBody = document.querySelectorAll("body")[0]
+    const treeDOMBody = document.querySelectorAll("body")[0];
 
-    beforeEach(angular.mock.module("App"))
-    beforeEach(inject(($rootScope, $compile, $location, $httpBackend) => {
-        const componentDOMelement = angular.element("<div ui-view></div>")
-        document.body.appendChild(componentDOMelement[0])
-        $compile(componentDOMelement)($rootScope.$new())
-
-        $location.url("/newtodo");
+    beforeEach(angular.mock.module("App"));
+    beforeEach(jsdomUIRouterScenario.build);
+    beforeEach(inject(($state, $rootScope) => {
+        $state.go("newtodo");
         $rootScope.$digest();
-    }))
-
-    afterEach(() => {
-        treeDOMBody.querySelectorAll("div[ui-view]")[0].remove()
-    })
+    }));
+    afterEach(jsdomUIRouterScenario.clean);
 
     it("should disable by default the submit button", () => {
         const submitButton = treeDOMBody.querySelector("#todo-submit");
         expect(submitButton.disabled).toBe(true);
     })
-    it("should enable the submit button only if the required fields was filled", inject(($rootScope) => {
+    it("should enable the submit button only if the required fields was filled", () => {
+        const formElement = angular.element(treeDOMBody.querySelector("form")).controller("todoForm").newtodoForm;
         const submitButton = treeDOMBody.querySelector("#todo-submit");
         const requiredElements = treeDOMBody.querySelectorAll("[required]");
 
         Array.from(requiredElements).forEach((field) => {
-            field.value = "some value";
+            formElement[field.name].$setViewValue("somevalue");
         });
-        submitButton.disabled = false;
-        console.log(treeDOMBody.outerHTML);
+
         expect(submitButton.disabled).toBe(false);
-    }))
+    })
 })
